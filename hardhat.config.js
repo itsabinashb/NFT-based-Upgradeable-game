ecdf374b574ae99d1b17285bf8a496389efd5b16
ABINASH BURMAN <abinash@hacek>
2023-04-29 22:41:31 +0530

    updated

--- hardhat.config.js
+++
@@ -1,7 +0,0 @@
-/** @type import('hardhat/config').HardhatUserConfig */
-require("@nomiclabs/hardhat-ethers")
-require("@openzeppelin/hardhat-upgrades")
-require("ethers")
-module.exports = {
-  solidity: "0.8.17",
-};
