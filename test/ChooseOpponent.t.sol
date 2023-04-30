7ff0093cf921c2ad115a8f83364ba7691b5c447f
Abinash Burman <itsabinashb@gmail.com>
2023-04-30 23:51:50 +0530

    test ongoing

---
+++ test/ChooseOpponent.t.sol
@@ -0,0 +1,19 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "forge-std/Test.sol";
+import "../src/Game.sol";
+
+contract ChooseOpponent is Test {
+    address me;
+    address opponent;
+    Game game;
+
+    function setUp() public {
+        game = new Game();
+        me = vm.addr(0x1);
+        opponent = vm.addr(0x2);
+        vm.deal(address(me), 5 ether);
+        vm.deal(address(opponent), 5 ether);
+    }
+} 
---
+++ test/GameInternal.t.sol
@@ -0,0 +1,27 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "forge-std/Test.sol";
+import {Game} from "../src/Game.sol";
+
+contract GameInternal is Game, Test {
+    Game game;
+    address me;
+    address user;
+
+    function setUp() public {
+        game = new Game();
+        uint256 myKey = 0x1111;
+        uint256 userKey = 0x2222;
+        me = vm.addr(myKey);
+        user = vm.addr(userKey);
+        vm.startPrank(me);
+    }
+
+    function test_tokenURI() external {
+        initialize("uri");
+        mint(user);
+        
+        
+    }
+}
