d792293513090a44ee83b458a7032c7cc1333cfc
Abinash Burman <itsabinashb@gmail.com>
2023-05-05 00:15:11 +0530

    complete

--- test/Game.t.sol
+++ test/Game.t.sol
@@ -10,24 +10,25 @@ contract TestGame is Test {
 
     function setUp() public {
         game = new Game(); // deployer = 0xb4c79daB8f259C7Aee6E5b2Aa729821864227e84
-        uint256 key = 0x1;  // setting private key
-        me = vm.addr(key);  // getting a new signer for the private key
-        vm.startPrank(me);  // setting this addr as msg.sender from now for every call untill stopPrank is called
+        uint256 key = 0x1; // setting private key
+        me = vm.addr(key); // getting a new signer for the private key
+        vm.startPrank(me); // setting this addr as msg.sender from now for every call untill stopPrank is called
     }
 
     function test_initialize() public {
-        //game.initialize("uri");
+        game.initialize();
         vm.expectRevert();
         vm.expectRevert();
     }
 
     function test_owner() public {
-        //game.initialize("uri");
+        game.initialize();
         assertEq(game.owner(), me);
     }
 
     function test_baseUri() public {
-       // game.initialize("uri");
+        game.initialize();
+        game.setbaseUri("uri");
         assertEq(game.baseUri(), "uri");
     }
 
@@ -35,7 +36,5 @@ contract TestGame is Test {
         assertEq(game.tokenId(), 0);
     }
 
-    function test_generateMonster() external{
-        
-    }
+    function test_generateMonster() external {}
 }
--- test/ProxyTest.t.sol
+++ test/ProxyTest.t.sol
@@ -6,28 +6,40 @@ import "../src/Game.sol";
 import "../src/MainProxy.sol";
 
 contract ProxyTest is Test {
-    MainProxy proxy;
-    Game game;
     address me;
-    UpgradedGame _game;
+    Game game;
+    MainProxy mainProxy;
+    Upgraded upgraded;
 
     function setUp() public {
         me = vm.addr(0x1);
-        game = new Game();
-        proxy = new MainProxy(address(game), abi.encodeWithSignature("initialize()", 0.001 ether));
-        _game = new UpgradedGame();
+        vm.deal(address(me), 5 ether);
+        vm.startPrank(address(me));
+        address gameImplementation = address(new Game());
+        console.log("game implementation address:", gameImplementation);
+        mainProxy = new MainProxy(
+            gameImplementation,
+            abi.encodeWithSignature("initialize()", 0.001 ether)
+        );
+        console.log("mainProxy address:", address(mainProxy));
+        game = Game(address(mainProxy));
+        console.log("game address:", address(game));
+        upgraded = new Upgraded();
+        console.log("address of Upgraded:", address(upgraded));
     }
 
     function test_proxy() public {
-        vm.startPrank(address(me));
-       // assertEq(proxy.monsterId(), 0);
-        //proxy.generateMonster{0.01 ether}();
-        //proxy.tokenId();
+        assertEq(game.monsterId(), 0);
+        // game.initialize();
+        game.upgradeTo(address(upgraded));
+        game.generateMonster{value: 0.01 ether}();
+        assertEq(game.monsterId(), 1);
+        // assertEq(game.getGreetings(),"You are doing well!");
     }
 }
 
-contract UpgradedGame is Game {
-    function grettings() public pure returns (string memory) {
+contract Upgraded is Game {
+    function getGreetings() public pure returns (string memory) {
         return "You are doing well!";
     }
 }
